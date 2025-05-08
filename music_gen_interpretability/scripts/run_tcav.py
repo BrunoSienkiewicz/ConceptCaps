005cfabbaeaff518628c50fd98e47132ac2597da
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-05-08 19:32:06 +0200

    update: moved to hydra script

--- music_gen_interpretability/scripts/run_tcav.py
+++ music_gen_interpretability/scripts/run_tcav.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import pandas as pd
 import matplotlib.pyplot as plt
+import hydra
 
 from functools import reduce
 
@@ -13,11 +14,12 @@ from transformers import MusicgenForConditionalGeneration, AutoProcessor
 
 from music_gen_interpretability.tcav.transform import select_samples, create_experimental_set
 from music_gen_interpretability.tcav.model import CustomMusicGen, ConceptClassifier
+from music_gen_interpretability.tcav.config import TCAVConfig
 
 def format_float(f):
     return float('{:.3f}'.format(f) if abs(f) >= 0.0005 else '{:.3e}'.format(f))
 
-def plot_tcav_scores(experimental_sets, tcav_scores):
+def plot_tcav_scores(experimental_sets, tcav_scores, layers):
     fig, ax = plt.subplots(1, len(experimental_sets), figsize = (25, 7))
 
     barWidth = 1 / (len(experimental_sets[0]) + 1)
@@ -49,76 +51,87 @@ def plot_tcav_scores(experimental_sets, tcav_scores):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-random_state = 42
-np.random.seed(random_state)
-torch.manual_seed(random_state)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-torch.cuda.manual_seed_all(random_state)
 
-processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
-model.to(device)
-model = model.half()  # Convert model weights to float16
-model.eval()
+@hydra.main(config_path="config", config_name="tcav_config")
+def run_tcav(cfg: TCAVConfig):
+    random_state = cfg.random_state
+    np.random.seed(random_state)
+    torch.manual_seed(random_state)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.cuda.manual_seed_all(random_state)
 
-data_path = "data/music_caps_transformed_instrument.csv"
+    # Load the model and processor
+    processor = AutoProcessor.from_pretrained(cfg.processor_name)
+    model = MusicgenForConditionalGeneration.from_pretrained(cfg.model_name)
 
-df = pd.read_csv(data_path)
+    model.to(device)
+    model = model.half()
+    model.eval()
 
-experimental_set = create_experimental_set(
-    concept_name="instrument_guitar",
-    genre="rock",
-    data_path=data_path,
-    batch_size=10,
-    num_samples=20,
-    experimental_set_size=5,
-)
-rock_samples = select_samples(
-    df=df,
-    concept="instrument_guitar",
-    genre="rock",
-    num_samples=20,
-)
-rock_text = [row["caption_without_genre"] for _, row in rock_samples.iterrows()]
-inputs = processor(
-    text=rock_text,
-    padding=True,
-    return_tensors="pt",
-)
-inputs = inputs.to(device)
+    # Load the dataset
+    df = pd.read_csv(cfg.data_path)
 
-# layers=['model.decoder.model.decoder.layers.0.fc1', 'model.decoder.model.decoder.layers.0.fc2', 'model.decoder.lm_heads.0', 'model.decoder.lm_heads.1']
-# layers=['model.decoder.model.decoder.layers.0.fc1']
-layers=['model.decoder.lm_heads.0', 'model.decoder.lm_heads.1']
+    # Create the experimental set
+    experimental_set = create_experimental_set(
+        concept_name=cfg.concept_name,
+        genre=cfg.genre,
+        data_path=cfg.data_path,
+        batch_size=cfg.batch_size,
+        num_samples=cfg.num_samples,
+        experimental_set_size=cfg.experimental_set_size,
+    )
+    genre_samples = select_samples(
+        df=df,
+        concept=cfg.concept_name,
+        genre=cfg.genre,
+        num_samples=cfg.num_samples,
+    )
+    genre_text = [row["caption_without_genre"] for _, row in genre_samples.iterrows()]
+    inputs = processor(
+        text=genre_text,
+        padding=True,
+        return_tensors="pt",
+    )
 
-custom_model = CustomMusicGen(model, processor, max_new_tokens=256)
-instrument_tcav = TCAV(
-    model=custom_model,
-    model_id="musicgen_small_instrument",
-    classifier=ConceptClassifier(),
-    layer_attr_method=LayerFeatureAblation(custom_model.forward, None),
-    layers=layers,
-    show_progress=True,
-)
+    inputs = inputs.to(device)
+    layers = cfg.layers
 
-n_groups = 10
+    custom_model = CustomMusicGen(model, processor, max_new_tokens=256)
+    instrument_tcav = TCAV(
+        model=custom_model,
+        model_id=cfg.model_id,
+        classifier=ConceptClassifier(),
+        layer_attr_method=LayerFeatureAblation(custom_model.forward, None),
+        layers=layers,
+        show_progress=True,
+    )
 
-layer_masks = []
-for layer in layers:
-    layer_shape = reduce(getattr, layer.split('.'), custom_model).weight.shape[0]
-    print(layer_shape)
-    layer_mask = torch.zeros(layer_shape).to(device)
-    group_size = layer_mask.shape[0] // n_groups
-    for i in range(n_groups+1):
-        layer_mask[i * group_size:(i + 1) * group_size] = i
-    layer_masks.append(layer_mask)
+    n_groups = cfg.n_groups
+    layer_masks = []
 
-layer_masks = (*layer_masks,)
+    for layer in layers:
+        layer_shape = reduce(getattr, layer.split('.'), custom_model).weight.shape[0]
+        layer_mask = torch.zeros(layer_shape).to(device)
+        group_size = layer_mask.shape[0] // n_groups
+        for i in range(n_groups+1):
+            layer_mask[i * group_size:(i + 1) * group_size] = i
+        layer_masks.append(layer_mask)
 
-tcav_scores = instrument_tcav.interpret(
-    inputs=(inputs.input_ids, inputs.attention_mask, None),
-    experimental_sets=experimental_set,
-    target=0,
-    layer_mask=layer_masks,
-)
+    layer_masks = (*layer_masks,)
+
+    tcav_scores = instrument_tcav.interpret(
+        inputs=(inputs.input_ids, inputs.attention_mask, None),
+        experimental_sets=experimental_set,
+        target=0,
+        layer_mask=layer_masks,
+    )
+
+    # Plot the TCAV scores
+    plot_tcav_scores(experimental_set, tcav_scores, layers)
+
+    # Save the TCAV scores to a CSV file
+    tcav_scores_df = pd.DataFrame.from_dict(tcav_scores, orient='index')
+    tcav_scores_df.to_csv(cfg.output_path, index=False)
+    print(f"TCAV scores saved to {cfg.output_path}")
+
