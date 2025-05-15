eb2538803915042cb3c55a82488431e148d0ab02
Bruno Sienkiewicz <bruno.sienkiewicz@xtb.com>
2025-05-15 16:59:11 +0200

    update: text conditioning dataset

--- music_gen_interpretability/data/generic_data_module.py
+++ music_gen_interpretability/data/generic_data_module.py
@@ -1,12 +1,14 @@
 import lightning as L
-import pandas as pd
 
 
 class GenericDataModule(L.LightningDataModule):
     def __init__(
-        self, data_path: str, batch_size: int, num_samples: int, experimental_set_size: int
+        self,
+        dataset: str,
+        processor: str,
+        batch_size: int,
     ):
         super().__init__()
-        self.data_path = data_path
+        self.dataset = dataset
+        self.processor = processor
         self.batch_size = batch_size
-        self.num_samples = num_samples
--- music_gen_interpretability/data/text_conditioning.py
+++ music_gen_interpretability/data/text_conditioning.py
@@ -1,17 +1,77 @@
 import pandas as pd
 
+from datasets import load_dataset
 from music_gen_interpretability.data.generic_data_module import GenericDataModule
 
 
-class TextConditioning(GenericDataModule):
-    def setup(self, stage: str = None) -> None:
-        super().setup(stage)
-        self.df = pd.read_csv(self.data_path)
-        self.df = self.df.sample(n=self.num_samples, random_state=self.random_state, replace=False)
-        self.df.reset_index(drop=True, inplace=True)
+def transform_concept(df, concept_list, concept_name):
+    _df = df.copy()
+    for concept in concept_list:
+        _df[f"is_{concept_name}_" + concept] = (
+            _df["caption"].str.contains(concept, case=False).astype(int)
+        )
+    return _df
 
-    def prepare_data(self, stage: str = None) -> None:
-        super().prepare_data(stage)
-        self.df = pd.read_csv(self.data_path)
-        self.df = self.df.sample(n=self.num_samples, random_state=self.random_state, replace=False)
-        self.df.reset_index(drop=True, inplace=True)
+
+def remove_concept(df, concept_list, concept_name):
+    _df = df.copy()
+    _df[f"caption_without_{concept_name}"] = _df["caption"]
+    for concept in concept_list:
+        _df[f"caption_without_{concept_name}"] = _df[
+            f"caption_without_{concept_name}"
+        ].str.replace(concept, "", case=False)
+    _df[f"caption_without_{concept_name}"] = _df[
+        f"caption_without_{concept_name}"
+    ].str.replace("  ", " ")
+    _df[f"caption_without_{concept_name}"] = _df[
+        f"caption_without_{concept_name}"
+    ].str.strip()
+    return _df
+
+
+class TextConditioning(GenericDataModule):
+    def __init__(
+        self,
+        dataset: str,
+        processor: str,
+        batch_size: int,
+        emotions: list[str],
+        instruments: list[str],
+        genres: list[str],
+    ):
+        super().__init__(dataset, processor, batch_size)
+        self.emotions = emotions
+        self.instruments = instruments
+        self.genres = genres
+
+    def prepare_data(self):
+        self.dataset = load_dataset(self.dataset)
+        self.dataset = self.dataset["train"].to_pandas()
+        self.dataset = self.dataset.drop(
+            columns=[
+                "start_s",
+                "end_s",
+                "audioset_positive_labels",
+                "author_id",
+                "is_balanced_subset",
+                "is_audioset_eval",
+            ]
+        )
+
+        self.dataset = transform_concept(self.dataset, self.emotions, "emotion")
+        self.dataset = transform_concept(self.dataset, self.instruments, "instrument")
+        self.dataset = transform_concept(self.dataset, self.genres, "genre")
+
+        is_any_genre = self.dataset.filter(like="is_genre_").sum(axis=1) > 0
+        is_any_instrument = self.dataset.filter(like="is_instrument_").sum(axis=1) > 0
+        is_any_emotion = self.dataset.filter(like="is_emotion_").sum(axis=1) > 0
+
+        self.dataset = self.dataset[
+            is_any_genre & is_any_instrument & is_any_emotion
+        ].reset_index(drop=True)
+
+        self.dataset_genre = remove_concept(self.dataset, self.genres, "genre")
+        self.dataset_instrument = remove_concept(
+            self.dataset, self.instruments, "instrument"
+        )
+        self.dataset_emotion = remove_concept(self.dataset, self.emotions, "emotion")
