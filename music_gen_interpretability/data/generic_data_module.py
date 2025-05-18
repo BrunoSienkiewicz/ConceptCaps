4dfc5436a5c8ca442b27e2a715e5ccb0033ec377
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-05-18 20:40:52 +0200

    add: generic methods

--- music_gen_interpretability/data/generic_data_module.py
+++ music_gen_interpretability/data/generic_data_module.py
@@ -22,3 +22,11 @@ class GenericDataModule:
     def setup(self, stage=None):
         # This method is called to set up the data for training, validation, and testing
         pass
+
+    def select_samples(self, num_samples: int):
+        # This method should return a subset of the dataset with the specified number of samples
+        pass
+
+    def select_random_samples(self, num_samples: int):
+        # This method should return a random subset of the dataset with the specified number of samples
+        pass
