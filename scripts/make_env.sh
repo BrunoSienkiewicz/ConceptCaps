31fc4c0b6059582259b23e02bf97d653c7f186e9
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-05-29 20:29:19 +0200

    update: split scripts

---
+++ scripts/make_env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH -J make_env
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=01:00:00
+#SBATCH -A plgxailnpw25-gpu-a100
+#SBATCH -p plgrid-gpu-a100
+#SBATCH --output="output.out"
+#SBATCH --error="error.err"
+
+cd "$SLURM_SUBMIT_DIR" || exit 1
+
+CONDA_DIR="/net/tscratch/people/plgbsienkiewicz/.conda"
+
+module load Miniconda3/23.3.1
+conda init --all
+conda config --add pkgs\_dirs $CONDA_DIR/pkgs
+conda config --add envs\_dirs $CONDA_DIR/envs
+
+srun conda env create -n "$(cat environment.yml | grep -E "name: " | cut -d " " -f 2)" -f environment.yml --yes
+conda activate "$(cat environment.yml | grep -E "name: " | cut -d " " -f 2)"
+srun poetry install
--- scripts/run_tcav.sh
+++ scripts/run_tcav.sh
@@ -16,15 +16,5 @@ cd "$SLURM_SUBMIT_DIR" || exit 1
 CONDA_DIR="/net/tscratch/people/plgbsienkiewicz/.conda"
 ENV_DIR="$CONDA_DIR/envs/$(cat environment.yml | grep -E "name: " | cut -d " " -f 2)"
 
-srun /bin/hostname
-module load Miniconda3/23.3.1
-conda init --all
-conda config --add pkgs\_dirs $CONDA_DIR/pkgs
-conda config --add envs\_dirs $CONDA_DIR/envs
-
-if [ ! -d "$ENV_DIR" ]; then
-  make env
-fi
-
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
 srun "$ENV_DIR/bin/python" src/scripts/run_tcav.py +preset=plgird +logger=wandb
