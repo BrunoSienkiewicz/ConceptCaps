a05f19218f7f7c42fee4775023906f05f99646b6
SuperNinja623 <bruno.sienkiewicz@gmail.com>
2025-11-06 10:48:49 +0100

    update: renamed paths

--- scripts/run_caption_test_generation.sh
+++ scripts/run_caption_test_generation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH -J run_caption_test
+#SBATCH -J run_caption_test_generation
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=8GB
@@ -52,4 +52,4 @@ export OUT_DIR="$OUT_DIR"
 export CONDA_DIR="$CONDA_DIR"
 
 mkdir -p "$HF_HOME"
-srun "$ENV_DIR/bin/python" src/scripts/caption/test_model.py "${HYDRA_OVERRIDES[@]}"
+srun "$ENV_DIR/bin/python" src/scripts/caption/test_generation.py "${HYDRA_OVERRIDES[@]}"
