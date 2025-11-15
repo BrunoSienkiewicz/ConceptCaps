49d446c55c41915ebca200373293d5804de90133
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-11-15 16:09:11 +0100

    update: logger run name

--- src/scripts/caption/run_fine_tuning.py
+++ src/scripts/caption/run_fine_tuning.py
@@ -26,9 +26,7 @@ def main(cfg: CaptionGenerationConfig) -> None:
     for logger in loggers:
         if isinstance(logger, pl.pytorch.loggers.wandb.WandbLogger):
             wandb.login()
-            run_name = logger.run_name if logger.run_name else None
-            if run_name is None:
-                run_name = f"caption-fine-tuning-{cfg.model.name}-{time.strftime('%Y%m%d-%H%M%S')}"
+            run_name = f"caption-fine-tuning-{cfg.model.name}-{time.strftime('%Y%m%d-%H%M%S')}"
             wandb.init(project=logger.project, name=run_name)
 
     device = torch.device(cfg.device)
