0f62bfb771978be67cacff8bf039fbef6f68c99c
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2026-01-11 18:59:17 +0100

    Subset dataset

--- scripts/run_tta.sh
+++ scripts/run_tta.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=01:00:00
+#SBATCH --time=06:00:00
 #SBATCH -A plgxailnpw25-gpu-a100
 #SBATCH -p plgrid-gpu-a100
 #SBATCH --output="logs/run_tta.out"
